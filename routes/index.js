a885804c185ea8e1654874d1694ff96d44ec645b
omer <omersh1111@gmail.com>
2021-09-08 03:40:26 +0300

    added /postsnumber route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const routes = express.Router();
 
 const posts = require("./posts/");
+const postsnumber = require("./posts/totalPostsNumber");
 const statistics = require("./statistics/");
 
 routes.use("/posts", posts);
 routes.use("/statistics", statistics);
+
+routes.get("/postsnumber", postsnumber);
 
 module.exports = routes;
