1ce957146e1a28764fc2bb8ab31adab305211e6a
omer <omersh1111@gmail.com>
2021-09-08 14:27:09 +0300

    added saving the runtime of getPosts, createPost in a new collection

--- routes/posts/createPost.js
+++ routes/posts/createPost.js
@@ -1,14 +1,17 @@
 const postsController = require("../../controllers/postsController");
+const statisticsController = require("../../controllers/statisticsController");
 
 const createPost = (req, res, next) => {
-  console.log("were hereeee");
+  const start = Date.now();
   const { creator, title, body } = req.body;
-  console.log(creator);
-  console.log(title);
-  console.log(body);
+  console.log(`creator: ${creator}, body: ${body}, title: ${title}`);
   const success = postsController.createPost(creator, title, body);
-  if (success) res.status(200).json({ success: "true", message: "success" });
-  else res.status(500).json({ success: "false", message: "error saving post" });
+  statisticsController.saveRuntimes("createPost", start);
+
+  if (success) {
+    res.status(200).json({ success: "true", message: "success" });
+  } else
+    res.status(500).json({ success: "false", message: "error saving post" });
 };
 
 module.exports = createPost;
--- routes/posts/getPosts.js
+++ routes/posts/getPosts.js
@@ -1,8 +1,10 @@
 const postsController = require("../../controllers/postsController");
+const statisticsController = require("../../controllers/statisticsController");
 
 const getPosts = async (req, res, next) => {
+  const start = Date.now();
   const posts = await postsController.getPosts();
-  console.log(posts);
+  statisticsController.saveRuntimes("getPost", start);
   res.send(posts);
 };
 
